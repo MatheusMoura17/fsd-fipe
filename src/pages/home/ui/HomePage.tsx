831837f92bcf1cb603c64ca2d0810fe0bd59f375
Matheus Moura <matheus.moura13@gmail.com>
2025-01-12 04:14:22 -0300

    feat: Add router to home page

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,11 +1,20 @@
+// Libs
+import {useNavigate} from 'react-router-dom';
+// Shared
 import {Button} from '@/shared/ui/Button';
 import {Layout} from '@/shared/ui/Layout';
 
 export const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/search');
+  };
+
   return (
     <Layout title="Tabela fipe">
       <p>Veja os preços médios de veículos usados no Brasil.</p>
-      <Button>Iniciar</Button>
+      <Button onClick={handleClick}>Iniciar</Button>
     </Layout>
   );
 };
