6441f3c0dd72f09a452324ed31aaee466857d4bf
Matheus Moura <matheusmoura@handtalk.me>
2025-02-13 01:36:11 -0300

    fix: Router root

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -5,22 +5,19 @@ import {HomePage} from '@/pages/home';
 import {SearchPage} from '@/pages/search';
 import {VehicleValue} from '@/pages/vehicle-value';
 
-export const router = createHashRouter(
-  [
-    {
-      path: '/',
-      element: <HomePage />,
-    },
-    {
-      path: '/search',
-      element: <SearchPage />,
-    },
-    {
-      path: '/vehicle-value',
-      element: <VehicleValue />,
-    },
-  ],
+export const router = createHashRouter([
   {
-    basename: import.meta.env.BASE_URL,
+    path: '/',
+    element: <HomePage />,
   },
-);
+  {
+    path: '/search',
+    element: <SearchPage />,
+  },
+  {
+    path: '/vehicle-value',
+    element: <VehicleValue />,
+  },
+]);
+
+console.log(import.meta.env.BASE_URL);
